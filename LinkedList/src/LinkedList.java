f2366267502affd4a4f83bba892d1c81e25399f0
kamal.kishor <kamal.kishor@RMKDT010.CHSPLDC.COM>
2016-08-10 16:50:09 +0530

    reverse linked list iterative commit

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,6 +1,8 @@
+
+
 //Simple LinkList program
 public class LinkedList {
-	Node head;
+	static Node head;
 	static class Node{
 		int data;
 		Node next;
@@ -10,20 +12,23 @@ public class LinkedList {
 		}
 	}
 	
-	public void printList(){
-		Node n=head;
+	//PrintList
+	public void printList(Node n){
+		//Node n=head;
 		while(n!=null){
 			System.out.print(n.data +"->");
 			n=n.next;
 		}
 	}
 	
+	//Push data at front
 	public void push(int new_data){
 		Node new_node=new Node(new_data);
 		new_node.next=head;
 		head=new_node;
 	}
 	
+	//Push data after a specific node
 	public void insertAfter(Node prev_node,int new_data){
 		if(prev_node==null){
 			System.out.println("The entered node is null");
@@ -32,6 +37,8 @@ public class LinkedList {
 		new_node.next=prev_node.next;
 		prev_node.next=new_node;
 	}
+	
+	//Append data at the last
 	public void append(int new_data){
 		Node new_node=new Node(new_data);
 		if(head==null){
@@ -44,6 +51,8 @@ public class LinkedList {
 		}
 		last.next=new_node;
 	}
+	
+	//
 	public void delete(int key){
 		Node temp=head;
 		Node prev=null;
@@ -77,8 +86,8 @@ public class LinkedList {
 		while(temp!=null){
 			temp=temp.next;
 			counter++;			
-			if(counter==position-1){
-				temp.data=temp.next.data;
+			if(counter==position){
+			
 				temp.next=temp.next.next;			
 				return;
 			}
@@ -92,60 +101,98 @@ public class LinkedList {
 	}
 	public int Length(){
 		int counter=0;
-		while(head!=null){
+		Node temp=head;
+		while(temp!=null){
 			counter++;
-			head=head.next;
+			temp=temp.next;
 			
 		}
 		return counter;
 		
 	}
+	
+	Node reverseIterative(Node node) {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+           //System.out.println("prev="+prev.data+" current="+current.data+" next="+next.data);
+            
+        }
+        node = prev;
+        return node;
+    }
+	
+	Node reverseRecursive(Node node){
+		
+		return node;
+	}
+			
+			
+		
 	/***********************************************************************/
 	public static void main(String args[]){
 		LinkedList ll=new LinkedList();
-		ll.head=new Node(1);
+		head=new Node(1);
 		Node second=new Node(2);
 		Node third=new Node(3);
-		ll.head.next=second;
+		head.next=second;
 		second.next=third;
-		ll.printList();System.out.println();
+		ll.printList(head);System.out.println();
 		
 		
 		System.out.println("\npush 4 and 5 at front");
-		ll.push(4);ll.push(5);ll.printList();System.out.println();		
+		ll.push(4);ll.push(5);ll.printList(head);System.out.println();		
 		
 		
 		System.out.println("\npush 6 after some node");
-		ll.insertAfter(ll.head.next.next, 6);ll.printList();System.out.println();
+		ll.insertAfter(ll.head.next.next, 6);ll.printList(head);System.out.println();
 		
 		
 		System.out.println("\nappend 7 and 8 at last");
-		ll.append(7);ll.append(8);;ll.printList();System.out.println();
+		ll.append(7);ll.append(8);;ll.printList(head);System.out.println();
+		
+		
 		
 		
 		System.out.println("\ndelete the head element");
-		ll.delete(9);ll.printList();System.out.println();
+		ll.delete(9);ll.printList(head);System.out.println();
 		
 		System.out.println("\ndelete any element");
-		ll.delete(2);ll.printList();System.out.println();
+		ll.delete(2);ll.printList(head);System.out.println();
 
 		System.out.println("\ndelete the last element");
-		ll.delete(16);ll.printList();System.out.println();
+		ll.delete(16);ll.printList(head);System.out.println();
 		
 		//delete element from specific position (head)
 		System.out.println("\ndelete from head :");
-		ll.deletePosition(0);ll.printList();System.out.println();
-
+		ll.deletePosition(0);ll.printList(head);System.out.println();
 
 		System.out.println("\ndelete from position 3 :");
-		ll.deletePosition(3);ll.printList();System.out.println();
-
+		ll.deletePosition(3);ll.printList(head);System.out.println();
 
 		System.out.println("\ndelete from extra position :");
-		ll.deletePosition(13);ll.printList();System.out.println();
+		ll.deletePosition(13);ll.printList(head);System.out.println();
 		
+		//Find the length of the linked list
 		System.out.println("\nLength of the linked list :");
 		System.out.println(ll.Length());
+		
+		//Find the reverse of the linked list using iterativr method
+		System.out.println("\nReverse of the linked list using Iterative Method:");
+		head=ll.reverseIterative(head);		
+		ll.printList(head);System.out.println();
+		
+		//Find the reverse of the linked list using recursive method
+		System.out.println("Reverse of the linked list using recursive method");
+		head=ll.reverseRecursive(head);
+		ll.printList(head);
+		
+		
 		
 		
 	}
