c490cabd94243cc35c7a8be7ed82eb3699895da1
kamal.kishor <kamal.kishor@RMKDT010.CHSPLDC.COM>
2016-08-12 14:19:24 +0530

    simpleDP largest contigupus sub array

--- Algorithms/src/dynamicProgramming/SimpleDP.java
+++ Algorithms/src/dynamicProgramming/SimpleDP.java
@@ -1,9 +1,37 @@
 package dynamicProgramming;
 
 public class SimpleDP {
+	public int minSteps(int n)
+	{
+		int c=0;
+		while(n>1){
+			if(n%3==0){
+				n=n/3;	c++;
+			}
+			else if(n%3==1){
+				n=n-1;	c++;
+			}
+			else if(n%2==0)
+			{
+				n=n/2;	c++;
+			}
+			else if(n%2==1){
+				n=n-1;	c++;
+			}
+			else{
+				n=n-1;
+				c++;
+			}
+		}
+		return c;
 
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		SimpleDP dp=new SimpleDP();
+		System.out.println(dp.minSteps(9999));
+
+
 
 	}
 
